734bca9c59d642b9ad3eacd73e6fcb4084477719
Steve Tarzia <steve@stevetarzia.com>
2012-03-04 18:04:49 -0600

    Initial Commit

---
+++ ClapIR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClapIR
+//
+//  Created by Stephen Tarzia on 3/4/12.
+//  Copyright (c) 2012 VaporStream, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
