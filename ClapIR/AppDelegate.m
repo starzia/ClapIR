966e3b5878b048f0e765736e3b394b8c0c696205
Steve Tarzia <stephen.tarzia@vaporstream.com>
2013-09-28 15:04:17 -0500

    fixed crash when resetting the app (on iOS 7)

--- ClapIR/AppDelegate.m
+++ ClapIR/AppDelegate.m
@@ -21,11 +21,12 @@
 -(void)reset{
     if( _mainView ){
         [_mainView.recorder stop];
-        [_mainView dismissModalViewControllerAnimated:NO];
+        _mainView = nil;
     }
     _mainView = [[MainViewController alloc] init];
-    [self.window.rootViewController presentModalViewController:_mainView animated:NO];
+    self.window.rootViewController = _mainView;
 }
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -33,13 +34,10 @@
     // Override point for customization after application launch.
     
     // set up GUI
-    // make blank root view on top of which we will present the main view modally (so it can be popped/pushed on reset)
-    self.window.rootViewController = [[UIViewController alloc] init];
-    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-    // push MainViewController
+    // show MainViewController
     [self reset];
     
     return YES;
